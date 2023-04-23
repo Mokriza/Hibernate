a9cb337c451851b955b893fdc56d70945fef3649
Andrey Belov <belovandr96@gmail.com>
2023-04-23 23:59:48 +0500

    add getConnection() method to Util class

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,23 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/pp_1_1_4";
+    private static final String USERNAME = "jdbc:mysql://localhost:3306/pp_1_1_4";
+    private static final String PASSWORD = "jdbc:mysql://localhost:3306/pp_1_1_4";
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Ошибка соединения с базой данных");
+        }
+        System.out.println("Соединение с БД установлено");
+        return connection;
+    }
+
+
 }
