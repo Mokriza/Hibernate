a2407941e2873d817a8d19c5ac89df7090ad9bb5
Andrey Belov <belovandr96@gmail.com>
2023-04-24 13:54:10 +0500

    add CRUD methods in DaoJDBCImplementation.java

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,95 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+
+    Connection connection = Util.getConnection();
+
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-
+        try (Statement statement = connection.createStatement()) {
+            String sqlCreateUsersTable = "CREATE TABLE pp_1_1_4.users " +
+                    "(id INT not NULL AUTO_INCREMENT, " +
+                    " name VARCHAR(50), " +
+                    " lastName VARCHAR (50), " +
+                    " age TINYINT(3) not NULL, " +
+                    " PRIMARY KEY (id))";
+            statement.execute(sqlCreateUsersTable);
+        } catch (SQLException e) {
+            System.out.println("При cоздании таблицы пользователей произошло исключение");
+        }
     }
 
     public void dropUsersTable() {
+        try (Statement statement = connection.createStatement()) {
+            String sqlDropUsersTable = "DROP TABLE pp_1_1_4.users";
+            statement.execute(sqlDropUsersTable);
+        } catch (SQLException e) {
+            System.out.println("При удалении таблицы пользователей произошло исключение");
+        }
 
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        String sql = "INSERT INTO PP_1_1_4.users (name, lastname, age) VALUES (?, ?, ?)";
+        try (PreparedStatement ps = connection.prepareStatement(sql);) {
+            ps.setString(1, name);
+            ps.setString(2, lastName);
+            ps.setByte(3, age);
 
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Ошибка при сохранении в БД нового пользователя");
+        }
     }
 
     public void removeUserById(long id) {
-
+        String sql = "DELETE FROM pp_1_1_4.users WHERE ID=?";
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setLong(1, id);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Ошибка при удалении пользователя из БД по ID");
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> result = new ArrayList<>();
+        String sql = "select id, name, lastname, age from pp_1_1_4.users";
+
+        try (Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("lastname"));
+                user.setAge(resultSet.getByte("age"));
+
+                result.add(user);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Ошибка при получении списка всех пользователей из БД");
+        }
+        return result;
     }
 
     public void cleanUsersTable() {
-
+        try (Statement statement = connection.createStatement()) {
+            String sqlDropUsersTable = "DELETE FROM pp_1_1_4.users";
+            statement.execute(sqlDropUsersTable);
+        } catch (SQLException e) {
+            System.out.println("При очистке таблицы пользователей произошло исключение");
+        }
     }
 }
