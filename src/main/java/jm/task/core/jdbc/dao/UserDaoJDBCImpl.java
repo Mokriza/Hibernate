b984ff73867d6b129c6a6f252d6427cae83c38ad
Andrey Belov <belovandr96@gmail.com>
2023-04-25 11:21:06 +0500

    sql fields move to class constants and methods parameters

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -11,19 +11,21 @@ public class UserDaoJDBCImpl implements UserDao {
 
     private Connection connection = Util.getConnection();
 
+    private final String sqlRemoveUser = "DELETE FROM pp_1_1_4.users WHERE ID=?";
+    private final String sqlSaveUser = "INSERT INTO PP_1_1_4.users (name, lastname, age) VALUES (?, ?, ?)";
+
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
         try (Statement statement = connection.createStatement()) {
-            String sqlCreateUsersTable = "CREATE TABLE IF NOT EXISTS pp_1_1_4.users (\n" +
+            statement.execute("CREATE TABLE IF NOT EXISTS pp_1_1_4.users (\n" +
                     "                    id INT not NULL AUTO_INCREMENT,  \n" +
                     "                    name VARCHAR(50),  \n" +
                     "                    lastName VARCHAR (50),\n" +
                     "                    age TINYINT(3) not NULL,\n" +
-                    "                    PRIMARY KEY (id))";
-            statement.execute(sqlCreateUsersTable);
+                    "                    PRIMARY KEY (id))");
         } catch (SQLException e) {
             System.out.println("При cоздании таблицы пользователей произошло исключение");
         }
@@ -31,8 +33,7 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void dropUsersTable() {
         try (Statement statement = connection.createStatement()) {
-            String sqlDropUsersTable = "DROP TABLE IF EXISTS pp_1_1_4.users";
-            statement.execute(sqlDropUsersTable);
+            statement.execute("DROP TABLE IF EXISTS pp_1_1_4.users");
         } catch (SQLException e) {
             System.out.println("При удалении таблицы пользователей произошла ошибка");
         }
@@ -40,8 +41,8 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        String sql = "INSERT INTO PP_1_1_4.users (name, lastname, age) VALUES (?, ?, ?)";
-        try (PreparedStatement ps = connection.prepareStatement(sql);) {
+
+        try (PreparedStatement ps = connection.prepareStatement(sqlSaveUser);) {
             ps.setString(1, name);
             ps.setString(2, lastName);
             ps.setByte(3, age);
@@ -53,8 +54,7 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void removeUserById(long id) {
-        String sql = "DELETE FROM pp_1_1_4.users WHERE ID=?";
-        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+        try (PreparedStatement ps = connection.prepareStatement(sqlRemoveUser)) {
             ps.setLong(1, id);
             ps.executeUpdate();
         } catch (SQLException e) {
@@ -64,10 +64,9 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public List<User> getAllUsers() {
         List<User> result = new ArrayList<>();
-        String sql = "select id, name, lastname, age from pp_1_1_4.users";
 
         try (Statement statement = connection.createStatement()) {
-            ResultSet resultSet = statement.executeQuery(sql);
+            ResultSet resultSet = statement.executeQuery("select id, name, lastname, age from pp_1_1_4.users");
             while (resultSet.next()) {
                 User user = new User();
                 user.setId(resultSet.getLong("id"));
@@ -86,8 +85,7 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void cleanUsersTable() {
         try (Statement statement = connection.createStatement()) {
-            String sqlDropUsersTable = "DELETE FROM pp_1_1_4.users";
-            statement.execute(sqlDropUsersTable);
+            statement.execute("DELETE FROM pp_1_1_4.users");
         } catch (SQLException e) {
             System.out.println("При очистке таблицы пользователей произошла ошибка");
         }
