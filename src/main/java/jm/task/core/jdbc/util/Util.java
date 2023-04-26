4463af55d19febe902ab05b019664f15c5c61126
Andrey Belov <belovandr96@gmail.com>
2023-04-26 17:33:13 +0500

    add getSessionFactory() method for Hibernate realisation

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,9 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,6 +12,7 @@ public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/pp_1_1_4";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
+
     public static Connection getConnection() {
         Connection connection = null;
         try {
@@ -17,6 +22,11 @@ public class Util {
             System.out.println("Ошибка соединения с БД");
         }
         return connection;
+    }
+    public static SessionFactory getSessionFactory() {
+        Configuration configuration = new Configuration().addAnnotatedClass(User.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        return sessionFactory;
     }
 
 
