bc1c0e9f97e93241d64ead6902bd3e98c72134ea
Andrey Belov <belovandr96@gmail.com>
2023-04-27 17:03:29 +0500

    add hibernate configuration in getSessionFactory() method

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,14 +3,18 @@ package jm.task.core.jdbc.util;
 import jm.task.core.jdbc.model.User;
 import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
-    private static final String URL = "jdbc:mysql://localhost:3306/pp_1_1_4";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/pp_1_1_4";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
 
@@ -19,7 +23,7 @@ public class Util {
     public static Connection getConnection() {
         Connection connection = null;
         try {
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            connection = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
             System.out.println("Соединение с БД установлено");
         } catch (SQLException e) {
             System.out.println("Ошибка соединения с БД");
@@ -30,8 +34,25 @@ public class Util {
     public static SessionFactory getSessionFactory() {
         if (sessionFactory == null) {
             try {
-                Configuration configuration = new Configuration().addAnnotatedClass(User.class);
-                sessionFactory = configuration.buildSessionFactory();
+                Configuration configuration = new Configuration();
+                Properties settings = new Properties();
+
+                settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/pp_1_1_4?autoReconnect=true&useSSL=false");
+                settings.put(Environment.USER, USERNAME);
+                settings.put(Environment.PASS, PASSWORD);
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+                configuration.setProperties(settings);
+                configuration.addAnnotatedClass(User.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
             } catch (HibernateException e) {
                 System.out.println("Ошибка создания Session Factory");
             }
