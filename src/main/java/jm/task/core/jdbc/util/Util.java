29ab82020f00d36aeb8064e7590e4ec1c1a05908
Andrey Belov <belovandr96@gmail.com>
2023-04-27 17:08:38 +0500

    fix Dialect Hibernate properties

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -41,7 +41,7 @@ public class Util {
                 settings.put(Environment.URL, "jdbc:mysql://localhost:3306/pp_1_1_4?autoReconnect=true&useSSL=false");
                 settings.put(Environment.USER, USERNAME);
                 settings.put(Environment.PASS, PASSWORD);
-                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
                 settings.put(Environment.SHOW_SQL, "true");
                 settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
                 settings.put(Environment.HBM2DDL_AUTO, "create-drop");
